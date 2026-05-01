69f57c8309b93cbdc2c281d52ef96c64f89b6218
Angelica_vargas2007 <avargas-2024a@corhuila.edu.co>
2026-05-01 16:09:59 -0500

    feat: agregar endpoint tabla de posiciones en torneo controller

--- backend/src/torneo/torneo.controller.ts
+++ backend/src/torneo/torneo.controller.ts
@@ -17,6 +17,11 @@ export class TorneoController {
     return this.torneoService.findAll();
   }
 
+  @Get(':id/tabla-posiciones')
+  tablaPosiciones(@Param('id', ParseIntPipe) id: number) {
+    return this.torneoService.tablaPosiciones(id);
+  }
+
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.torneoService.findOne(id);
